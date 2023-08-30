46c85284a1c0394f6596cb6675e4960ae9408bef
Guilherme <guimoura.oliveira@gmail.com>
2023-08-30 14:21:10 -0300

    "commita"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Intro1 from './components/Intro1';
-import Intro2 from './components/Intro2';
-import Intro3 from './components/Intro3';
-
+import Heading from "./components/Heading";
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Footer />
-      <Intro1 />
-      <Intro2 />
-      <Intro3 />
-    </div>
-  );
+  return (
+    <div className="App">
+      <Heading firstName="Bob" />
+      <Heading firstName="Any name other than Bob" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <h1>Hello there indeed, {props.name} and {props.color} </h1>
+  );
+}
+export default Header;
