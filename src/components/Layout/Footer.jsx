a99f4c3e592726324e398d94bc75f4dcb99b787c
Guilherme <guimoura.oliveira@gmail.com>
2023-09-12 22:27:50 -0300

    "n/a"

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,13 +1,18 @@
 import styles from './Footer.module.css'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
-
+import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <div className={styles.footerContainer}>
-        < FaLinkedin className={styles.icons} size={40} />
-        < FaGithub className={styles.icons} size={40} />
-
+        <div className={styles.iconsContainer}>
+        <Link to={'https://www.linkedin.com/in/guilherme-moura-oliveira/'} target='blank'>
+          < FaLinkedin className={styles.icons} size={40} />
+        </Link>
+        <Link to={'https://github.com/guimouraO1'} target='blank'>
+          < FaGithub className={styles.icons} size={40} />
+        </Link>
+        </div>
     </div>
   );
 }
